99624ef7ad748e05bea56eb19795757a1bbe3d2f
Mritunjay Kumar <mritunjay.kumar@rackspace.com>
2019-01-07 13:24:56 -0800

    Rollup to monplat Kafka topics (all 5 rollup levels) are working.

--- src/main/java/com/rackspacecloud/metrics/rollup/config/KafkaStreamsConfiguration.java
+++ src/main/java/com/rackspacecloud/metrics/rollup/config/KafkaStreamsConfiguration.java
@@ -37,7 +37,7 @@ public class KafkaStreamsConfiguration {
     @Bean
     @Profile("development")
     KafkaStreams devKafkaStreams() {
-        setStreamsConfiguration(true);
+        setStreamsConfiguration(false);
         final KafkaStreams streams = new KafkaStreams(topology, config);
 
         return streams;
